de48cffc2aad65d2820c5bd4ba8685c662559f82
Matias <matiasezequielsmania@gmail.com>
2026-04-17 12:30:27 -0300

    seccion de empresas

--- Front/src/app/pages/home/home.ts
+++ Front/src/app/pages/home/home.ts
@@ -6,4 +6,47 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  currentIndex = 0;
+  visibleCards = 2;
+
+  proyectos = [
+    {
+      titulo: 'Sistema de gestión',
+      descripcion: 'Plataforma desarrollada en C# y SQL Server para control financiero.',
+      color: 'text-emerald-400'
+    },
+    {
+      titulo: 'Landing empresarial',
+      descripcion: 'Sitio corporativo moderno para consultora de recursos humanos.',
+      color: 'text-sky-300'
+    },
+    {
+      titulo: 'Registro de materias',
+      descripcion: 'Aplicación en Python con SQLite para seguimiento académico.',
+      color: 'text-purple-300'
+    },
+    {
+      titulo: 'Simulador financiero',
+      descripcion: 'Simulador de plazo fijo y cotización en Java.',
+      color: 'text-pink-300'
+    }
+  ];
+
+  next() {
+    if (this.currentIndex < this.proyectos.length - this.visibleCards) {
+      this.currentIndex++;
+    }
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  get transformValue() {
+    return `translateX(-${this.currentIndex * 50}%)`;
+  }
+
+}
